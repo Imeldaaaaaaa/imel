fe7f9a69ddb00a1271419d9496fef300a87e09c3
Imeldaaaaaaa <imeldadede02@gmail.com>
2025-01-02 20:27:46 +0700

    Mengedit  routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('home'); // Halaman utama
+});
+
+Route::get('/about', function () {
+    return view('about'); // Halaman about
+});
+
+Route::get('/profile', function () {
+    return view('profile'); // Halaman profile
+});
+
+Route::get('/contact', function () {
+    return view('contact'); // Halaman contact
+});
